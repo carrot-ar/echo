8789fbb71e54014aaffe5dd96a2aa6a47ecc1159
Conor Landry <clandry94@ufl.edu>
2017-10-17 16:00:39 -0400

    add deployment things

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM partlab/ubuntu
+
+MAINTAINER Régis Gaidot <regis@partlab.co>
+
+ENV DEBIAN_FRONTEND noninteractive
+ENV INITRD No
+ENV LANG en_US.UTF-8
+ENV GOVERSION 1.9
+ENV GOROOT /opt/go
+ENV GOPATH /root/.go
+
+RUN cd /opt && wget https://storage.googleapis.com/golang/go${GOVERSION}.linux-amd64.tar.gz && \
+    tar zxf go${GOVERSION}.linux-amd64.tar.gz && rm go${GOVERSION}.linux-amd64.tar.gz && \
+    ln -s /opt/go/bin/go /usr/bin/ && \
+    mkdir $GOPATH
+
+RUN go get github.com/gorilla/websocket
+RUN go get github.com/senior-buddy/buddy
+RUN cd /root/.go/src/github.com/ && mkdir clandry94
+RUN cd /root/.go/src/github.com/clandry94/ && git clone https://github.com/clandry94/echo
+
+RUN cp /root/.go/src/github.com/clandry94/echo/home.html /home.html
+
+CMD ["/usr/bin/go"]
+ENTRYPOINT ["/usr/bin/go", "run", "/root/.go/src/github.com/clandry94/echo/echo.go"]
--- echo.go
+++ echo.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-  "github.com/senior-buddy/buddy"
-  "fmt"
+	"fmt"
+	"github.com/senior-buddy/buddy"
 )
 
 /*
   Requests are in the form of
 
   {
-      "endpoint": "test",
+			"session_token: "de0u21ejdd12d",
+			"endpoint": "test",
       "params": {
           "test1": "result1",
           "test2": "result2"
       }
   }
 
- */
-
+*/
 
 // Controller implementation
 type EchoController struct{}
 
 func (c *EchoController) Echo(req *buddy.Request) {
-  fmt.Println("I'm in the Echo method!")
+	fmt.Println("I'm in the Echo method!")
 
-  // respond here once the responder is implemented!
+	// respond here once the responder is implemented!
 }
 
 func (c *EchoController) PrintParams(req *buddy.Request) {
-  fmt.Println(req.Params)
+	fmt.Println(req.Params)
 }
 
 func main() {
 
-  // Register endpoints here in the form of endpoint, controller, method
-  buddy.Add("echo", EchoController{}, "Echo")
-  buddy.Add("print_params", EchoController{}, "PrintParams")
+	// Register endpoints here in the form of endpoint, controller, method
+	buddy.Add("echo", EchoController{}, "Echo")
+	buddy.Add("print_params", EchoController{}, "PrintParams")
 
-  // Run the server and serve traffic
-  buddy.Run()
+	// Run the server and serve traffic
+	buddy.Run()
 }
