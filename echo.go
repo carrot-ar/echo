b8a3f4b3c74b02093eb7323e34bf4ff7a9723caf
Conor Landry <clandry94@ufl.edu>
2017-10-25 15:31:44 -0400

    update image

--- echo.go
+++ echo.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/senior-buddy/buddy"
+	"github.com/carrot-ar/carrot"
 )
 
 /*
@@ -22,13 +22,11 @@ import (
 // Controller implementation
 type EchoController struct{}
 
-func (c *EchoController) Echo(req *buddy.Request, res *buddy.Responder) {
-	fmt.Println("I'm in the Echo method!")
-
-	res.Broadcast <- []byte([]byte(fmt.Sprintf("%v",req.Origin)))
+func (c *EchoController) Echo(req *carrot.Request, res *carrot.Broadcast) {
+	res.Send([]byte([]byte(fmt.Sprintf("%v", req.Params["rick"]))))
 }
 
-func (c *EchoController) PrintParams(req *buddy.Request, res *buddy.Responder) {
+func (c *EchoController) PrintParams(req *carrot.Request, res *carrot.Broadcast) {
 	fmt.Println(req.Params)
 }
 
@@ -36,7 +34,7 @@ type EchoStreamController struct {
 	count int
 }
 
-func (c *EchoStreamController) EchoStreamer(req *buddy.Request, res *buddy.Responder) {
+func (c *EchoStreamController) EchoStreamer(req *carrot.Request, res *carrot.Broadcast) {
 	fmt.Println(c.count)
 	c.count += 1
 }
@@ -44,10 +42,10 @@ func (c *EchoStreamController) EchoStreamer(req *buddy.Request, res *buddy.Respo
 func main() {
 
 	// Register endpoints here in the form of endpoint, controller, method
-	buddy.Add("echo", EchoController{}, "Echo", false)
-	buddy.Add("print_params", EchoController{}, "PrintParams", false)
-	buddy.Add("echo_streamer", EchoController{}, "EchoStreamer", true)
+	carrot.Add("echo", EchoController{}, "Echo", true)
+	carrot.Add("print_params", EchoController{}, "PrintParams", false)
+	carrot.Add("echo_streamer", EchoController{}, "EchoStreamer", true)
 
 	// Run the server and serve traffic
-	buddy.Run()
+	carrot.Run()
 }
